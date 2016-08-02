b7ef108738f3be4ee8e5239f272e27ac2cdd5d4f
jlengstorf <jason.lengstorf@copterlabs.com>
2016-08-02 18:30:36 +0200

    docs(server): added comments

--- app/server.js
+++ app/server.js
@@ -5,11 +5,17 @@
  */
 const Hapi = require('hapi');
 
+/**
+ * Creates a Hapi server that’s ready to start.
+ * @param  {number} port the port for the API to connect on (default `5000`)
+ * @return {Server}      an instance of hapi’s `Server`
+ */
 function createServer(port) {
   const server = new Hapi.Server();
 
   server.connection({ port });
 
+  // Register each of the routes as a plugin & set the prefix for the endpoint.
   server.register([
     {
       register: require('./routes/status'),
